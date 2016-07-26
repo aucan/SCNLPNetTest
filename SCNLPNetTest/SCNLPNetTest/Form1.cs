9dc370df8062bdf01567d0c13ceed136fe771611
AUcan <AUcan@AUCAN>
2016-07-26 15:24:02 +0300

    lemmaorstem

--- SCNLPNetTest/SCNLPNetTest/Form1.cs
+++ SCNLPNetTest/SCNLPNetTest/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows.Forms;
 
 namespace SCNLPNetTest
@@ -16,6 +18,39 @@ namespace SCNLPNetTest
         {
             txtOutputen.Text = sc.getLemmas(txtInputen.Text);
             txtOutputtr.Text = trlm.getLemmas(txtInputtr.Text);
-        } 
+        }
+
+        private void btnGetTdkLemma_Click(object sender, EventArgs e)
+        {
+            List<String> lemmas = new List<string>();
+            foreach (string item in File.ReadAllLines(@"C:\Users\AUcan\Desktop\wordlist.txt"))
+            {
+                lemmas.Add(GetOnlyLemma(item));
+                Console.WriteLine(lemmas.Count);
+            }
+            File.WriteAllLines(@"C:\Users\AUcan\Desktop\wordlistlemmas2.txt", lemmas);
+        }
+
+        private string GetOnlyLemma(string item)
+        {
+            string strlemmas = trlm.getLemmas(item);
+            string result="";
+
+            foreach (string itm in strlemmas.Split())
+            {
+                result += itm.Split(new char[] { '|' })[0]+" ";
+            }
+            return result.Trim();
+        }
+
+        private void frmTest_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmTest_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
